50ab2a6609c93a392c078faaae36128150ae4c30
Mark Vadeika <markvadeika@gmail.com>
2022-05-09 18:53:48 -0400

    added watering can

--- src/main/java/com/craftschemers/ranchcraft/item/custom/WateringCanItem.java
+++ src/main/java/com/craftschemers/ranchcraft/item/custom/WateringCanItem.java
@@ -1,14 +1,17 @@
 package com.craftschemers.ranchcraft.item.custom;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Fertilizable;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
-
-import java.util.Objects;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldEvents;
 
 public class WateringCanItem extends Item {
 
@@ -21,6 +24,9 @@ public class WateringCanItem extends Item {
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
 
+        World world = context.getWorld();
+        boolean used = useOnFertilizable(context.getWorld(), context.getBlockPos());
+
         if (context.getWorld().isClient()) {
             PlayerEntity player = context.getPlayer();
 
@@ -28,9 +34,51 @@ public class WateringCanItem extends Item {
                 return super.useOnBlock(context);
             }
 
-            player.playSound(SoundEvents.ITEM_BUCKET_EMPTY, SoundCategory.BLOCKS, 1f, 1f);
+            BlockPos blockPos = context.getBlockPos();
+            if (used) {
+                world.playSound(
+                        blockPos.getX(),
+                        blockPos.getY(),
+                        blockPos.getZ(),
+                        SoundEvents.ITEM_BUCKET_EMPTY,
+                        SoundCategory.BLOCKS,
+                        1f,
+                        1f,
+                        true
+                );
+            } else {
+                world.playSound(
+                        blockPos.getX(),
+                        blockPos.getY(),
+                        blockPos.getZ(),
+                        SoundEvents.BLOCK_DISPENSER_FAIL,
+                        SoundCategory.BLOCKS,
+                        1f,
+                        1f,
+                        true
+                );
+            }
 
+        } else {
+            world.syncWorldEvent(WorldEvents.BONE_MEAL_USED, context.getBlockPos(), 0);
         }
+
         return super.useOnBlock(context);
     }
+
+    public static boolean useOnFertilizable(World world, BlockPos pos) {
+
+        Fertilizable fertilizable;
+        BlockState blockState = world.getBlockState(pos);
+        if (blockState.getBlock() instanceof Fertilizable && (fertilizable = (Fertilizable) blockState.getBlock()).isFertilizable(world, pos, blockState, world.isClient)) {
+            if (world instanceof ServerWorld) {
+                if (fertilizable.canGrow(world, world.random, pos, blockState)) {
+                    fertilizable.grow((ServerWorld) world, world.random, pos, blockState);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
 }
